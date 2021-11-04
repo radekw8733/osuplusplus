a8ad38b4c00c647764de813eefad035b001b9eea
Radosław Wolański <radekw8733@gmail.com>
2021-11-04 21:52:03 +0100

    Basic window

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include "SDL.h"
 
 int main() {
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *window = SDL_CreateWindow("osu!reloaded",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,480,480,0);
+    SDL_Delay(5000);
+    SDL_Quit();
     return 0;
 }
