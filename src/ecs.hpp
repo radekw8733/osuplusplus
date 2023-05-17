5772c888005165ddb8c02eab1a5ff598abe70d42
Radosław Wolański <radekw8733@gmail.com>
2023-05-17 19:17:23 +0200

    Moved from C++ to Rust

--- src/ecs.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class Node {
-    public:
-        virtual void start() = 0;
-        virtual void update(float delta) = 0;
-        virtual void onMouseClick(int xMouse, int yMouse) {};
-        virtual ~Node() {};
-};
